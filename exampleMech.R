6401abe233ca702f63a9a7d28685e3190597407c
andresfpc <andresfpalaciosc@gmail.com>
2019-02-08 21:54:47 -0500

    Second commit

--- exampleMech.R
+++ exampleMech.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/andre")
+setwd("C:/Users/andre/Desktop")
 library("KDViz")
 
 myKeywords <- c("impact fatigue","composite materials", "dcb", "track growth rate")
@@ -14,17 +14,26 @@ TermFreqByWord(termFreqTable = termFreqTable, word = "docume")
 
 myDTM <- DTMFromCorpus(corpus = myCorpus, rowNames = row.names(myBibData))
 
-mpaWords <- matrix.corpus.mpa(myCorpus, fmin = 8, cmin = 1)
+mpaWords <- matrix.corpus.mpa(myCorpus, fmin = 4, cmin = 1)
 mpaWords$Words
 
-myKDSummary <- KDSummary(matrix.mpa = mpaWords, groupSize = 10)
+myKDSummary <- KDSummary(matrix.mpa = mpaWords, groupSize = 4)
 myKDSummary$clustTable
 myKDSummary$wordClasses
 
 myWordGroupDTM <- GroupDTM(dtm = myDTM, kdSummary = myKDSummary)
 
 KDViz(groupDTM = myWordGroupDTM, group = 1, export = TRUE)
-KDViz(groupDTM = myWordGroupDTM, group = 12, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 2, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 3, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 4, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 5, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 6, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 7, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 8, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 9, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 10, export = TRUE)
+KDViz(groupDTM = myWordGroupDTM, group = 11, export = TRUE)
 
 myKDSummary$wordClasses[myKDSummary$wordClasses$Class == 11, ]
 
